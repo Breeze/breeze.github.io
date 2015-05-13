4f18491dae7989f90e03bc3c39157852b239d439
Jay Traband <jtraband@comcast.net>
2015-05-13 03:47:39 -0700

    ...

--- js/highlightselection.js
+++ js/highlightselection.js
@@ -8,19 +8,3 @@ $(window).on('load', function() {
     
 });
 
-$("a:not('.dropdown-toggle')").click(function(event) {
-    return false;
-    // var url = event.target.pathname;
-    // if (url == location.pathname) return false;
-
-    // if ends with '/', don't add the hash, let browser handle normally
-    // if (url.substr(url.length - 1) == '/') return true;
-
-    // if (url && !url.indexOf("#") == 0) {
-        // url = "#" + url;
-    // }
-    // if (url && url != location.hash) {
-        // location.hash = url;
-    // }
-    // return false;
-});
