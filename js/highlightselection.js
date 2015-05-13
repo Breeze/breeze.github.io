b7cf639810203d04d4e39e0235ceba190a361c27
Jay Traband <jtraband@comcast.net>
2015-05-13 14:37:12 -0700

    .

--- js/highlightselection.js
+++ js/highlightselection.js
@@ -1,16 +1,19 @@
 $(window).on('load', function() {
+    // select the current li
     var pathName = location.pathname;
     var pathParts = pathName.split("/");
     var lastPart = pathParts[pathParts.length-1];
     $("li.active").removeClass("active");
     var li = $('a[href="' + lastPart + '"]').parent();
     li.addClass("active");
-
+    
+    // force any parents that are dropdowns to drop down.
     var dropdowns = li.parents('.dropdown-menu');
     dropdowns.each(function(ix, dd) {
-        dd.prev().dropdown('toggle');
+        $(dd).prev().dropdown('toggle');
     });
     
+    // scroll the current li to the top.
     $('#leftnav').animate({
        scrollTop: li.offset().top
     });
