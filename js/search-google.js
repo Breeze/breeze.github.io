b5ae9f447aa43cce4eb8923f45f0c1de3e7ce174
Jay Traband <jtraband@comcast.net>
2015-05-26 10:59:54 -0700

    google search - first pass

---
+++ js/search-google.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function() {
+
+    $("#searchform").submit(function(event) {
+
+        var term = $("#searchterm").val();
+        if (!term) return false;
+
+        var newUrl = window.location.protocol + "//" + window.location.host + "/doc-main/search-results.html?q=" + term;
+
+        // var url = "http://doc-main/search-results.html?q=" + term;
+        window.location.href = newUrl;
+        return false;
+
+        function escapeHtml(string) {
+            return String(string).replace(/[&<>"'\/]/g, function(s) {
+                return entityMap[s];
+            });
+        }
+    });
+
+});
